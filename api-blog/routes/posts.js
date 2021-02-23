2b879c95934491ee684d0eabcb00a2bf61428c7f
GastonRR <gaston.requena@gmail.com>
2021-02-23 10:17:09 -0300

    setting routes

--- api-blog/routes/posts.js
+++ api-blog/routes/posts.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET */
+
+router.get('/');
+router.get('/:id');
+
+/* POST */
+
+router.post('/');
+
+/* PATCH */
+
+router.patch('/:id');
+
+/* DELETE */
+
+router.patch('/:id');
 
 module.exports = router;
