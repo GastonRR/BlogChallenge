7ae2b8e3445c35d7a2523e30fdf571cb5b387ace
GastonRR <gaston.requena@gmail.com>
2021-02-24 14:43:48 -0300

    add switch and routes

--- my-blog/src/components/app/App.js
+++ my-blog/src/components/app/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+
+/* Pages component */
+import CreationPost from '../pages/form-creation/pageCreation'
+
 function App() {
   return (
     <div className="App">
-      holi
+    <Switch>
+      {/* <Route exact path='/' component={}/> */}
+      {/* <Route path='/detail/:id' component={}/> */}
+      <Route  path='/create/post' component={CreationPost}/>
+      {/* <Route path='/edit/post/:id' component={}/> */}
+      {/* <Route path='*' component={}></Route> */}
+    </Switch>
     </div>
   );
 }
